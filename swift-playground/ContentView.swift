adb70042e2ae52a0c8d29dad596f7006179e80f2
かずのこ <k42un0k0.put1put1@gmail.com>
2021-08-17 10:51:49 +0900

    Initial Commit

---
+++ swift-playground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-playground
+//
+//  Created by 白鳥天太 on 2021/08/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-playground/swift_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_playgroundApp.swift
+//  swift-playground
+//
+//  Created by 白鳥天太 on 2021/08/17.
+//
+
+import SwiftUI
+
+@main
+struct swift_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
